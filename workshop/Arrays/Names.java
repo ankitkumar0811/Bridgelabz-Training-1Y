b784e669ec8fc9978d27c693ddad881324c873c1
ankitkumar0811 <ay5180922@gmail.com>
2026-01-30 09:58:49 +0530

    Arrays

---
+++ workshop/Arrays/Names.java
@@ -0,0 +1,12 @@
+class Names{
+    public static void main(String[] args){
+	   String [] names=new String[4];
+	   names[0]="DIVyansh";
+	   names[1]="luv";
+	   names[2]="ankit";
+       names[3]="gaurang";
+       for(int i=0;i<4;i++){
+           System.out.print(names[i]+",");
+	   }
+	}
+}	
